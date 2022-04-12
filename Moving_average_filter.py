0f8388e58a25cacf5f0e3ef55dc27bb9b5f1d198
Jung Sunghun <65435447+seoungheong@users.noreply.github.com>
2022-04-12 21:28:46 +0900

    Add files via upload

---
+++ Moving_average_filter.py
@@ -0,0 +1,68 @@
+from collections import deque
+import matplotlib.pyplot as plt
+import random
+
+def Moving_average_filter( before_data_deque, before_Moving_average, new_data ):
+
+    if len(before_data_deque)!=0:
+
+        size = len(before_data_deque)
+
+        if(before_data_deque.maxlen > size):
+            size += 1
+            oldest_data = before_data_deque.popleft()
+            before_data_deque.appendleft(oldest_data)
+
+        else:
+            oldest_data = before_data_deque.popleft()
+
+    else:
+        size = 1
+        oldest_data = 0
+
+
+    Moving_average = before_Moving_average + (new_data - oldest_data)/size
+
+    before_data_deque.append(new_data)
+
+    return Moving_average, before_data_deque
+
+#####################################################################################
+#ex).1
+data_queue =deque( maxlen = 10 )
+
+def random_num(count):
+    w = 0 + 10 * random.random()
+    z = count + w
+    return z
+
+x=[]
+y=[]
+means=[]
+mean=0
+
+count = 0
+
+for i in range(1,101):
+
+    x.append(i)
+    num = random_num(count)
+    y.append(num)
+    mean, data_queue = Moving_average_filter(data_queue, mean, num)
+    means.append(mean)
+
+
+
+    if i % 5 == 0:
+        count += 5
+
+    print(data_queue)
+
+plt.plot(x,y,'ro--', label='Measured')
+plt.plot(x,means, 'g^', label='Average')
+
+plt.legend()
+plt.show()
+
+#####################################################################################
+
